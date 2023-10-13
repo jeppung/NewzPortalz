d0fea079f4df5e1b907052b1b224373183f28b7f
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-13 11:05:11 +0700

    feat: adding initial input image funtionality in post modal

--- src/components/postModal.tsx
+++ src/components/postModal.tsx
@@ -13,6 +13,7 @@ const PostModal = ({ onClose, onSuccess }: IPostModalProps) => {
     const [title, setTitle] = useState<string>("")
     const [description, setDescription] = useState<string>("")
     const [type, setType] = useState<string>("free")
+    const [image, setImage] = useState<File>()
 
     const slugHandler = () => {
         return title.toLowerCase().split(" ").join("-")
@@ -49,18 +50,20 @@ const PostModal = ({ onClose, onSuccess }: IPostModalProps) => {
         }
     }
 
+    const imageHandler = (data: FileList) => {
+        setImage(data[0])
+    }
+
 
     return (
         <dialog open className='w-full h-full bg-slate-500/50 justify-center items-center flex'>
             <div className='bg-white w-[60%] h-[90%] rounded-md p-16 relative overflow-auto'>
                 <button onClick={() => onClose()} className='absolute right-5 top-3'>X</button>
                 <h1 className='text-3xl font-bold'>Create new post</h1>
-                <form action="#" className='mt-7 flex flex-col gap-y-2  h-full' onSubmit={(e) => submitHandler(e)}>
+                <form action="#" className='mt-7 flex flex-col gap-y-3 ' onSubmit={(e) => submitHandler(e)}>
                     <div className='flex flex-col gap-y-2'>
                         <label htmlFor="title" className='text-lg'>Title</label>
                         <input onChange={(e) => setTitle(e.target.value)} type="text" name="title" id="title" className='border-2 p-2 rounded-md' placeholder='Post title' required />
-                    </div>
-                    <div>
                     </div>
                     <div className='flex flex-col gap-y-2'>
                         <label htmlFor="slug" className='text-lg'>Slug</label>
@@ -69,6 +72,18 @@ const PostModal = ({ onClose, onSuccess }: IPostModalProps) => {
                     <div className='flex flex-col gap-y-2'>
                         <label htmlFor="description" className='text-lg'>Description</label>
                         <input onChange={(e) => setDescription(e.target.value)} type="text" name="description" id="description" className='border-2 p-2 rounded-md' placeholder='Post description' required />
+                    </div>
+                    <div className='flex justify-between'>
+                        <div className='flex flex-col gap-y-2'>
+                            <label htmlFor="thumbnail" className='text-lg'>Thumbnail</label>
+                            <input type="file" name="thumbnail" id="thumbnail" required onChange={(e) => e.target.files?.length === 1 && imageHandler(e.target.files)} />
+                        </div>
+                        {
+                            image && <div>
+                                <img src={URL.createObjectURL(image!)} className='h-52' alt="" />
+                            </div>
+                        }
+
                     </div>
                     <div className='flex flex-col gap-y-2'>
                         <label htmlFor="type" className='text-lg'>Type</label>
@@ -85,7 +100,7 @@ const PostModal = ({ onClose, onSuccess }: IPostModalProps) => {
                                 resize: false,
                                 placeholder: "Write your content here..."
                             }}
-                            onEditorChange={(content, editor) => {
+                            onEditorChange={(content) => {
                                 setBody(content)
                             }}
                         />
