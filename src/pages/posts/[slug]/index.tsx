d3c698c61676373f4064ef060ee753b89e4c5fc5
jeppung <jeppung@gmail.com>
2023-10-14 11:50:14 +0700

    feat: add moment library, modifying post detail view

--- src/pages/posts/[slug]/index.tsx
+++ src/pages/posts/[slug]/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import parse from 'html-react-parser';
 import { HTMLReactParserOptions, Element } from 'html-react-parser';
+import moment from 'moment';
 
 const PostDetail = ({ }) => {
     const [post, setPost] = useState<IPost | null>(null)
@@ -41,28 +42,33 @@ const PostDetail = ({ }) => {
             <Navbar />
             <main className='max-w-7xl mx-auto py-10'>
                 <section className='border-b-2 pb-2 flex justify-between items-end'>
-                    <div>
-                        <h1 className='text-3xl'>{post?.title}</h1>
-                        <h2 className='text-xl text-gray-500'>{post?.description}</h2>
+                    <div className='max-w-4xl'>
+                        <h1 className='text-3xl font-bold'>{post?.title}</h1>
+                        <h2 className='text-sm text-gray-500'>{post?.description}</h2>
                     </div>
                     <div>
                         <p>Author: {post?.user.name}</p>
                     </div>
                 </section>
                 <section className='flex justify-center mt-5'>
-                    <img src={post?.thumbnail} />
+                    <img src={post?.thumbnail} className='object-cover' width={400} />
                 </section>
                 <section className='mt-10 content-wrapper'>
                     {post && parse(post.body, options)}
                 </section>
-                <section className='flex gap-x-5 justify-end mt-16'>
-                    <div className='flex gap-x-2'>
-                        <p>{post?.likes}</p>
-                        <button>Like</button>
+                <section className='flex gap-x-5 justify-between mt-16'>
+                    <div>
+                        <p>Created at: {moment(post?.createdAt).format("MMMM DD YYYY HH:mm")}</p>
                     </div>
-                    <div className='flex gap-x-2'>
-                        <p>{post?.shares}</p>
-                        <button>Share</button>
+                    <div className='flex gap-x-5'>
+                        <div className='flex gap-x-2'>
+                            <p>{post?.likes}</p>
+                            <button>Like</button>
+                        </div>
+                        <div className='flex gap-x-2'>
+                            <p>{post?.shares}</p>
+                            <button>Share</button>
+                        </div>
                     </div>
                 </section>
             </main>
