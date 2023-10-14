b33c595189e1831f5dbfb0eed875f85579a52786
jeppung <jeppung@gmail.com>
2023-10-14 12:16:25 +0700

    feat: modifying admin posts table view, adding more data to db.json

--- src/pages/admin/posts/index.tsx
+++ src/pages/admin/posts/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/navbar'
 import PostModal from '@/components/postModal'
 import { IUser } from '@/pages/login'
+import moment from 'moment'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
@@ -92,6 +93,9 @@ const AdminPosts = () => {
                                 <th className='p-2 text-start'>Title</th>
                                 <th className='p-2 text-start'>Slug</th>
                                 <th className='p-2 text-start'>Author</th>
+                                <th className='p-2 text-start'>Category</th>                                
+                                <th className='p-2 text-start'>Created At</th>
+                                <th className='p-2 text-start'>Updated At</th>
                                 <th className='p-2 text-start'>Actions</th>
                             </tr>
                         </thead>
@@ -104,6 +108,10 @@ const AdminPosts = () => {
                                             <td className='p-2'>{post.title}</td>
                                             <td className='p-2'>{post.slug}</td>
                                             <td className='p-2'>{post.user.name}</td>
+                                            <td className='p-2'>{post.category}</td>
+                                            <td className='p-2'>{moment(post.createdAt).format("MM-DD-YYYY HH:mm")}</td>
+                                            <td className='p-2'>{moment(post.updatedAt).format("MM-DD-YYYY HH:mm")}</td>
+                                        
                                             <td>
                                                 <div className='flex justify-around'>
                                                     <Link href={`/posts/${post.slug}`}>See</Link>
