57ce714559fddf6e05e73e4b7c1ab5e9a6a517ce
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-16 13:15:36 +0700

    feat: modifying minor view style in subscription page

--- src/pages/subscription/index.tsx
+++ src/pages/subscription/index.tsx
@@ -9,15 +9,15 @@ const Subscription = () => {
             <main className='bg-[#112D4E] flex-1 py-[50px]'>
                 <section className='max-w-7xl mx-auto'>
                     <div className='bg-[#F9F7F7] h-[738px] w-full rounded-2xl py-[100px] px-[50px] flex'>
-                        <div className='w-full flex flex-col justify-center'>
+                        <div className='w-full flex flex-col justify-center gap-y-3'>
                             <Image src="/group-team.png" alt='group-team' width={500} height={0} />
-                            <div className='mt-5'>
+                            <div className=''>
                                 <h1 className='text-5xl font-bold'>Be part of us</h1>
                                 <p className='mt-1 text-lg'>Lets read and read and read more</p>
                             </div>
                         </div>
                         <div className='w-full'>
-                            <h1 className='text-3xl text-center'>Subscription</h1>
+                            <h1 className='text-[36px] text-center'>Subscription</h1>
                             <div className='flex gap-x-5'>
                                 <div className='bg-[#DEDEDE] w-[277px] h-[413px] p-[28px] rounded-2xl flex flex-col mt-[40px] shadow-lg border border-black'>
                                     <div className='flex-1'>
