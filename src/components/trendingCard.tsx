bc509ff6e29b6e5a461f6ed98b60e02899573138
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-16 10:53:51 +0700

    feat: modifying title and description style

--- src/components/trendingCard.tsx
+++ src/components/trendingCard.tsx
@@ -5,23 +5,23 @@ import { title } from 'process'
 import React from 'react'
 
 export interface ITrendingCardProps {
-    premium: boolean
-    thumbnail: string
-    title: string
-    description: string
-    slug: string
+  premium: boolean
+  thumbnail: string
+  title: string
+  description: string
+  slug: string
 }
 
-const TrendingCard = ({premium, thumbnail, title, slug}: ITrendingCardProps) => {
+const TrendingCard = ({ premium, thumbnail, title, slug, description }: ITrendingCardProps) => {
   return (
-    <div className="bg-[#FDF0F0] shadow-xl  w-[350px] rounded-2xl relative">
-        { premium && <Image src="/premium_tag.svg" className="absolute right-[-30px] top-[-30px]" width={100} height={100} alt="terserah"/>}
-        <Image src={thumbnail} className="rounded-tl-2xl rounded-tr-2xl w-full h-[200px] object-cover" width={300} height={100} alt="terserah"/>
-        <div className="p-4 flex flex-col">
-            <h2 className="text-xl w-full font-bold">{title}</h2>
-            <p className="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.  ltrices mauris. Maecenas vitae mattis tellus.</p>
-            <Link href={`/posts/${slug}`} className="mt-2 bg-[#132043] text-white text-xs rounded-md py-[5px] px-[15px] self-end">See more</Link>
-        </div>
+    <div className="bg-[#FDF0F0] shadow-xl  w-[350px] rounded-2xl relative flex flex-col">
+      {premium && <Image src="/premium_tag.svg" className="absolute right-[-30px] top-[-30px]" width={100} height={100} alt="terserah" />}
+      <Image src={thumbnail} className="rounded-tl-2xl rounded-tr-2xl w-full h-[200px] object-cover" width={300} height={100} alt="terserah" />
+      <div className="p-4 flex flex-col flex-1 ">
+        <p className="text-xl  truncate font-bold text-ellipsis overflow-hidden">{title} </p>
+        <p className="mt-1 text-xs flex-1 line-clamp-3">{description}</p>
+        <Link href={`/posts/${slug}`} className="mt-2 bg-[#132043] text-white text-xs rounded-md py-[5px] px-[15px] self-end">See more</Link>
+      </div>
     </div>
   )
 }
