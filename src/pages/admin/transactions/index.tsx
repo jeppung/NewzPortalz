c1245a61f07771db564e86bd65798c28ef71377f
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-17 15:03:55 +0700

    feat: adding filter in admin transactions page

--- src/pages/admin/transactions/index.tsx
+++ src/pages/admin/transactions/index.tsx
@@ -3,13 +3,27 @@ import { ISubsTransaction } from '@/pages/subscription'
 import moment from 'moment'
 import React, { useEffect, useState } from 'react'
 
+
+interface ITransactionFilter {
+  status: string
+  sort: "createdAt"
+  order: string
+  date: string
+}
+
 const AdminTransaction = () => {
 
   const [transactions, setTransactions] = useState<ISubsTransaction[]>()
+  const [filter, setFilter] = useState<ITransactionFilter>({
+    status: "",
+    sort: "createdAt",
+    order: "desc",
+    date: ""
+  })
 
   const getTransactionsData = async () => {
     try {
-      const res = await fetch("http://localhost:6969/transactions?_expand=user")
+      const res = await fetch(`http://localhost:6969/transactions?_expand=user&createdAt_like=${filter.date}&status_like=${filter.status}&_sort=${filter.sort}&_order=${filter.order}`)
       if (!res.ok) {
         console.log(res.statusText)
       }
@@ -54,13 +68,32 @@ const AdminTransaction = () => {
 
   useEffect(() => {
     getTransactionsData()
-  }, [])
+  }, [filter])
 
   return (
     <>
       <Navbar />
       <main className='max-w-7xl mx-auto pt-10'>
-        <h1 className='text-3xl'>Transactions</h1>
+        <div className='flex items-center justify-between'>
+          <h1 className='text-3xl'>Transactions</h1>
+          <div className='flex gap-x-5'>
+            <select name="transaction_filter" id="transaction_filter" className='p-2 rounded-md border' value={filter.status} onChange={(e) => setFilter({ ...filter, status: e.target.value })}>
+              <option value="">All</option>
+              <option value="cancelled">Cancelled</option>
+              <option value="cancelled">Completed</option>
+              <option value="pending">Pending</option>
+              <option value="waiting payment">Waiting payment</option>
+            </select>
+            <input type="date" name="date" id="date border" onChange={(e) => {
+              if (e.target.valueAsDate === null) return setFilter({ ...filter, date: "" })
+              setFilter({ ...filter, date: moment(e.target.valueAsDate).format("YYYY-MM-DD").toString() })
+            }} />
+            <select name="order" id="order" className='p-2 rounded-md border' value={filter.order} onChange={(e) => setFilter({ ...filter, order: e.target.value })}>
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
+          </div>
+        </div>
         <section className='mt-5 '>
           <table className='w-full table border border-collapse'>
             <thead className='bg-green-500 text-white'>
@@ -88,7 +121,7 @@ const AdminTransaction = () => {
                       <td className='p-2'>{moment(transaction.createdAt).format("MM-DD-YYYY HH:mm")}</td>
                       <td className='p-2'>{moment(transaction.updatedAt).format("MM-DD-YYYY HH:mm:ss")}</td>
                       {
-                        transaction.status === "waiting payment" || transaction.status === "pending" && <td className='p-2'>
+                        (transaction.status === "waiting payment" || transaction.status === "pending") && <td className='p-2'>
                           <div className='flex gap-x-2'>
                             <button onClick={() => actionHandler(transaction, "reject")} className='bg-red-400 text-white px-5 rounded-md'>Rej</button>
                             <button onClick={() => actionHandler(transaction, "accept")} className='bg-green-400 text-white px-5 rounded-md'>Acc</button>
