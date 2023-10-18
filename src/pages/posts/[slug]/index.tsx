374c440958622ad7545d6a1773096a7fc8a8c85f
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-18 14:39:19 +0700

    feat: configure functionality of post share and like

--- src/pages/posts/[slug]/index.tsx
+++ src/pages/posts/[slug]/index.tsx
@@ -4,8 +4,8 @@ import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import parse from 'html-react-parser';
 import moment from 'moment';
-import {AiFillHeart, AiOutlineHeart, AiOutlineShareAlt} from "react-icons/ai"
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { AiFillHeart, AiOutlineHeart, AiOutlineShareAlt } from "react-icons/ai"
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { getCookie, setCookie } from 'cookies-next';
 import { IReadHistory, IUser } from '@/pages/login';
 
@@ -18,6 +18,11 @@ const PostDetail = ({ }) => {
     const router = useRouter()
 
     const getPostDetail = async () => {
+        const user = getUserData()
+
+        let dataIndex = user?.readHistory?.findIndex(data => data.slug === router.query.slug)
+        setIsLike(user!.readHistory![dataIndex!].isLike)
+
         try {
             const res = await fetch(`http://localhost:6969/posts?slug=${router.query.slug}&_expand=user`)
             if (!res.ok) {
@@ -32,17 +37,120 @@ const PostDetail = ({ }) => {
         }
     }
 
-    const likeHandler = async () => {
-        if(isLike) {
+    const likeHandler = () => {
+        if (isLike) {
             setLikesCounter(likesCounter - 1)
-        }else{
+            updateLike(likesCounter - 1)
+        } else {
             setLikesCounter(likesCounter + 1)
+            updateLike(likesCounter + 1)
         }
         setIsLike(!isLike)
     }
 
+    const shareHandler = () => {
+        setSharesCounter(sharesCounter + 1)
+        updateShare()
+    }
+
+    const getUserData = () => {
+        const cookie = getCookie("userData")
+        if (cookie !== undefined) {
+            return JSON.parse(cookie) as IUser
+        }
+    }
+
+    const updateShare = async () => {
+        const user = getUserData()
+        const dataIndex = user?.readHistory?.findIndex(data => data.slug === post?.slug)
+        if (dataIndex !== -1 && user!.readHistory![dataIndex!].isShare === false) {
+            user!.readHistory![dataIndex!].isShare = true
+            try {
+                const res = await fetch(`http://localhost:6969/users/${user!.id}`, {
+                    method: "PATCH",
+                    body: JSON.stringify({
+                        readHistory: user!.readHistory
+                    } as IUser),
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                })
+
+                if (!res.ok) return alert("Error updating share")
+
+                const newUserData = await res.json()
+                setCookie("userData", newUserData)
+            } catch (e) {
+                return alert(`Error updating share ${e}`)
+            }
+        }
+
+        try {
+            const res = await fetch(`http://localhost:6969/posts/${post?.id}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    shares: post!.shares + 1,
+                    updatedAt: new Date().toISOString()
+                } as IPost),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+
+            if (!res.ok) return alert("Error updating share")
+        } catch (e) {
+            return alert(`Error updating share ${e}`)
+        }
+
+    }
+
+    const updateLike = async (count: number) => {
+        const user = getUserData()
+        const dataIndex = user?.readHistory?.findIndex(data => data.slug === post?.slug)
+
+        if (dataIndex === -1) return
+
+        user!.readHistory![dataIndex!].isLike = !isLike
+
+        try {
+            const res = await fetch(`http://localhost:6969/users/${user!.id}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    readHistory: user!.readHistory
+                } as IUser),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+
+            if (!res.ok) return alert("Error updating like")
+
+            const newUserData = await res.json()
+            setCookie("userData", newUserData)
+        } catch (e) {
+            return alert(`Error updating like ${e}`)
+        }
+
+        try {
+            const res = await fetch(`http://localhost:6969/posts/${post?.id}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    likes: count,
+                    updatedAt: new Date().toISOString()
+                } as IPost),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+
+            if (!res.ok) return alert("Error updating like")
+        } catch (e) {
+            return alert(`Error updating like ${e}`)
+        }
+    }
+
     const updateUserHistory = async (data: IReadHistory[], user: IUser) => {
-        try{
+        try {
             const res = await fetch(`http://localhost:6969/users/${user.id}`, {
                 method: "PATCH",
                 body: JSON.stringify({
@@ -52,51 +160,48 @@ const PostDetail = ({ }) => {
                     "Content-type": "application/json"
                 }
             })
-            if(!res.ok) return alert("Error adding post to history")
+            if (!res.ok) return alert("Error adding post to history")
             const newUserData = await res.json()
             setCookie("userData", newUserData)
-        }catch(e){
+        } catch (e) {
             return alert("Error adding post to history")
         }
     }
 
     const addToHistory = async () => {
-        const cookie = getCookie("userData")
-        if(cookie !== undefined){
-            const user = JSON.parse(cookie) as IUser
-            const date = new Date().toISOString()  
-            
-            let data: IReadHistory = {
-                id: 1,
-                slug: router.query.slug as string,
-                isLike: false,
-                isShare: false,
-                createdAt: "",
-                updatedAt: ""
-            }
+        const user = getUserData()
+        const date = new Date().toISOString()
 
-            if(user.readHistory == null) {   
+        let data: IReadHistory = {
+            id: 1,
+            slug: router.query.slug as string,
+            isLike: false,
+            isShare: false,
+            createdAt: "",
+            updatedAt: ""
+        }
+
+        if (user!.readHistory == null) {
+            data.createdAt = date
+            data.updatedAt = date
+            updateUserHistory([data], user!)
+        } else {
+            const isExistIndex = user!.readHistory.findIndex((data) => data.slug === router.query.slug)
+            if (isExistIndex === -1) {
+                data.id = user!.readHistory.length + 1
                 data.createdAt = date
-                data.updatedAt = date    
-                updateUserHistory([data], user)
-            }else{
-                const isExistIndex = user.readHistory.findIndex((data) => data.slug === router.query.slug)
-                if(isExistIndex === -1) {
-                    data.id = user.readHistory.length + 1
-                    data.createdAt = date
-                    data.updatedAt = date    
-                    updateUserHistory([...user.readHistory, data], user)
-                }else{
-                    user.readHistory[isExistIndex].updatedAt = date
-                    updateUserHistory([...user.readHistory], user)
-                }
+                data.updatedAt = date
+                updateUserHistory([...user!.readHistory, data], user!)
+            } else {
+                user!.readHistory[isExistIndex].updatedAt = date
+                updateUserHistory([...user!.readHistory], user!)
             }
         }
     }
 
 
     useEffect(() => {
-        if(router.query.slug !== undefined){
+        if (router.query.slug !== undefined) {
             getPostDetail()
             addToHistory()
         }
@@ -128,13 +233,13 @@ const PostDetail = ({ }) => {
                     <div className='flex gap-x-5'>
                         <div className='flex gap-x-1'>
                             <p>{likesCounter}</p>
-                            <button onClick={likeHandler}>{isLike ? <AiFillHeart color="red"/> : <AiOutlineHeart/>}</button>
+                            <button onClick={likeHandler}>{isLike ? <AiFillHeart color="red" /> : <AiOutlineHeart />}</button>
                         </div>
                         <CopyToClipboard text={`http://localhost:3000/posts/${router.query.slug}`} onCopy={() => alert("Link copied")}>
-                           <div className='flex gap-x-1'>
+                            <div className='flex gap-x-1'>
                                 <p>{sharesCounter}</p>
-                                <button onClick={() => setSharesCounter(sharesCounter+1)}><AiOutlineShareAlt/></button>
-                           </div>
+                                <button onClick={shareHandler}><AiOutlineShareAlt /></button>
+                            </div>
                         </CopyToClipboard>
                     </div>
                 </section>
