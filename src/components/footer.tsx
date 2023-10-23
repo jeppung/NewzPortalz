813b11a1293580d6d8f1792b6b75c45a4cfd1626
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-23 13:11:20 +0700

    feat: modifying footer style for mobile view

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,17 +6,19 @@ import { BsFacebook } from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer className="bg-[#112D4E] p-4">
-        <div className=" max-w-7xl mx-auto flex justify-between">
-          <div className="text-white text-xs">
-            <h1>Copyright &#169; 2023 NewzPotalz | Designed by Kevin</h1>
-          </div>
-          <div className="text-white flex gap-x-2">
-              <Link href={"/#instagram"}><AiOutlineInstagram size={20}/></Link>
-              <Link href={"/#twitter"}><AiOutlineTwitter size={20}/></Link>
-              <Link href={"/#facebook"}><BsFacebook size={18}/></Link>            
-          </div>
+      <div className=" max-w-7xl mx-auto flex justify-between items-center">
+        <div className="text-white text-xs md:flex md:gap-x-1">
+          <p>Copyright &#169; 2023 NewzPotalz</p>
+          <p className='hidden md:flex'>|</p>
+          <p>Designed by Kevin</p>
         </div>
-      </footer>
+        <div className="text-white flex gap-x-2">
+          <Link href={"/#instagram"}><AiOutlineInstagram size={20} /></Link>
+          <Link href={"/#twitter"}><AiOutlineTwitter size={20} /></Link>
+          <Link href={"/#facebook"}><BsFacebook size={18} /></Link>
+        </div>
+      </div>
+    </footer>
   )
 }
 
